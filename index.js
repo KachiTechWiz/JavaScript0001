db7909ae530e039bcdf64026b39cc85c325cc8a9
KachiTechWiz <onyekachiekechi4@gmail.come>
2022-11-21 06:38:51 +0100

    git

---
+++ index.js
@@ -0,0 +1,14 @@
+$('body').ready(function(){
+    $('button').click(function(){
+        $('input').show();
+    });   
+});
+
+$('body').ready(function(){
+    $('.stop').click(function(){
+        $('input').hide();
+    });   
+});
+
+
+
